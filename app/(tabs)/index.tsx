2e43aab98cb09e3978e49911e8226e4752277047
Kevin Kim <kimanikevin254@gmail.com>
2024-07-21 15:24:04 +0300

    Replace text with button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,16 @@
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { useUserStore }  from '../../store/user'
 
 export default function Index() {
   const removeUser = useUserStore(state => state.removeUser);
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text
+      <Button
+        title='Log Out'
         onPress={() => {
           removeUser();
-        }}>
-        Sign Out
-      </Text>
+        }}
+      />
     </View>
   );
 }
